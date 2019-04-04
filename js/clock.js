7d2ef17e25f68c321701bde556fba442fd44a3c9
Kim Backman <voittamatonkulho@gmail.com>
2019-04-04 09:33:46 +0300

    Kopioitu edellisen repositorion tiedostot.

---
+++ js/clock.js
@@ -0,0 +1,26 @@
+
+var clock = new Vue({
+  el: '#clock',
+  data: {
+    time: ''
+  }
+});
+
+var timerID = setInterval(paivitaAika, 1000);
+paivitaAika();
+
+function paivitaAika() {
+  var cd = new Date();
+  clock.time = kelloTaulu(cd.getHours(), 2) +':'+
+              kelloTaulu(cd.getMinutes(), 2) +':'+
+              kelloTaulu(cd.getSeconds(), 2);
+};
+
+
+function kelloTaulu(numero, digi) {
+  var nolla = '';
+  for(var i = 0; i < digi; i++) {
+    nolla += '0';
+  }
+  return (nolla + numero).slice(-digi);
+}
