bd7ea8e4edb8cc088e638ec39e98c224f704ab17
Kim Backman <voittamatonkulho@gmail.com>
2019-04-09 14:56:38 +0300

    Siirretty tuntien arvot taulukkoon.

--- js/clock.js
+++ js/clock.js
@@ -7,19 +7,18 @@ var clock = new Vue({
 });
 
 
-var timerID = setInterval('paivitaAika(utc)', 1000);
+var timerID = setInterval('paivitaAika(utc)', 500);
 paivitaAika();
 
 function paivitaAika(tunnit) {
   var cd = new Date();
   if (tunnit){
-    if(tunnit == -125 || tunnit == -65){
-      cd.setMinutes(cd.getMinutes() - 30)
-      tunnit = (tunnit+5)/10
+    if(tunnit == -12.5 || tunnit == -6.5){
+      cd.setMinutes(cd.getMinutes() - 30);
+      tunnit += 1
     }
-    else if(tunnit == 05 || tunnit == 15 || tunnit == 25 ||tunnit == 35 || tunnit == 75){
-      cd.setMinutes(cd.getMinutes() + 30)
-      tunnit = (tunnit-5)/10
+    else if(tunnit == 0.5 || tunnit == 1.5 || tunnit == 2.5 ||tunnit == 3.5 || tunnit == 7.5){
+      cd.setMinutes(cd.getMinutes() + 30);
     }
     cd.setHours(cd.getHours() + tunnit)
   }
@@ -45,13 +44,17 @@ var zone = new Vue({
   },
   methods: {
     onChange(event) {
-      var value = event.target.value
-      value = parseInt(value)
-      utc = value;
+      var tuntiTaulukko = [-15,-14,-13,-12.5,-12,-11,-10,-9,-8,-7,-6.5,-6,-5,-4,-3,-2,-1,0,0.5,1,1.5,2,2.5,3,3.5,4,5,6,7,7.5,8,9,10];
+      utc = tuntiTaulukko[parseFloat(event.target.value)];
+      console.log(utc);
+      console.log(utc + utc);
+      utc = parseFloat(utc);
+
       return utc;
     }
   }
 })
+
 
 //Tämän alle laitetaan geolocation - ominaisuus
 
@@ -75,8 +78,8 @@ var zone = new Vue({
 		this.pituus = position.coords.longitude;
 	 }
 	}
-if (pituus > 18) {
-  document.getElementById("demo").innerHTML = "Good day!";
-}
+//if (pituus > 18) {
+//  document.getElementById("demo").innerHTML = "Good day!";
+//}
 
 });
